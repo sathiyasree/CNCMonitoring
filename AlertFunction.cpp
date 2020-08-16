6ea6fc3cd7284c1d51c2c9a7db853de7a371d766
Sathiyasree <68428401+sathiyasree@users.noreply.github.com>
2020-08-16 22:45:38 +0530

    Update AlertFunction.cpp

--- AlertFunction.cpp
+++ AlertFunction.cpp
@@ -1,4 +1,21 @@
 #include "AlertFunction.hpp"
+
+void alertUser(DiagData alertReason)
+{
+	/* give data to the alert output type in required format*/
+	m_alertReason.push_back(alertReason);
+}
+
+void clearPreviousAlerts()
+{
+	m_alertReason.clear();
+}
+	
+bool doesSystemNeedAttention()
+{
+	return(m_alertReason.size() != 0);
+} 
+
 std::string IAlert::getdiagnosisData()
 {
 
