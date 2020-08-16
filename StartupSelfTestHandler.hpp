bed7a1f36ed796859e4a8fa6341297839034bb1e
Sathiyasree <68428401+sathiyasree@users.noreply.github.com>
2020-08-16 21:46:19 +0530

    Create StartupSelfTestHandler.hpp

---
+++ StartupSelfTestHandler.hpp
@@ -0,0 +1,23 @@
+#ifndef __STARTUPSELFTESTHANDLER_H__
+
+#include "IcncMonitor.h"
+
+enum SelfTestStatusCode
+{
+	C_NoData = 0x00,
+	C_ControllerBoard_NOK = 0x01,
+	C_ConfigDataCorrupted = 0x02,
+	C_AllOk = 0xFF
+};
+
+class StartupSelfTestHandler
+{
+public: 
+	StartupSelfTestHandler(unsigned int selfTestStatuscode);
+	void updateStartupSelfTestStatus(unsigned int selfTestStatuscode);
+
+	void validateCncData();
+private:
+	unsigned int m_selfTestStatuscode;
+};
+#endif // !__STARTUPSELFTESTHANDLER_H__
