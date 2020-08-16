ef36396a8c115097fa679a8060334042393c1500
Sathiyasree <68428401+sathiyasree@users.noreply.github.com>
2020-08-16 21:44:15 +0530

    Create OperatingTempHandler.hpp

---
+++ OperatingTempHandler.hpp
@@ -0,0 +1,22 @@
+#ifndef __OPERATINGTEMPHANDLER_H__
+
+#include "IcncMonitor.h"
+
+class OperatingTempHandler
+{
+public:
+
+	OperatingTempHandler() = default;
+	~OperatingTempHandler() = default;
+
+	OperatingTempHandler(float currentTemp, float maxlimit);
+	void updateOperatingTemp(float currentTemp);
+	void changeOperatingTempLimit(float maxLimit);
+
+	void validateCncData();
+private:
+	float m_currentTemp;
+	float m_maxLimit;
+};
+
+#endif // !__OPERATINGTEMPHANDLER_H__
