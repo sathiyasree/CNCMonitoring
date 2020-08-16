62aef65ae7d82ffc80907af16ade5eda9cedbb26
Sathiyasree <68428401+sathiyasree@users.noreply.github.com>
2020-08-16 21:48:00 +0530

    Create PartDimVariationHandler.cpp

---
+++ PartDimVariationHandler.cpp
@@ -0,0 +1,25 @@
+#include"PartDimVariationHandler.h"
+
+PartDimVariationHandler::PartDimVariationHandler(float currentPartDimvariation, float maxLimit)
+{
+	m_currentPartDimVariation = currentPartDimvariation;
+	m_maxLimit = maxLimit;
+}
+
+void PartDimVariationHandler::updatePartDimVariation(float currentPartDimvariation)
+{
+	m_currentPartDimVariation = currentPartDimvariation;
+	validateCncData();
+}
+
+void PartDimVariationHandler::validateCncData()
+{
+	if (m_currentPartDimVariation > m_maxLimit)
+	{
+		// call Alert function
+	}
+}
+void PartDimVariationHandler::changeOperationTempLimit(float maxLimit)
+{
+	m_maxLimit = maxLimit;
+}
