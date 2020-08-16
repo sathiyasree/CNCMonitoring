f4d071bb07dc16c4ea1cb1fc704f7f9499fe5bd4
Sathiyasree <68428401+sathiyasree@users.noreply.github.com>
2020-08-16 21:43:21 +0530

    Create OperationDurationHandler.hpp

---
+++ OperationDurationHandler.hpp
@@ -0,0 +1,21 @@
+#ifndef __OPERATIONDURATIONHANDLER_H__
+
+#include "IcncMonitor.h"
+
+class OperationDurationHandler
+{
+public:
+	OperationDurationHandler() = default;
+	~OperationDurationHandler() = default;
+
+	OperationDurationHandler(unsigned int operationDuration, unsigned int maxLimit);
+	void updateOperationDuration(unsigned int operationDuration);
+	void changeOperationDurationLimit(unsigned int maxLimit);
+
+	void validateCncData();
+
+private:
+	unsigned int m_operationDuration;
+	unsigned int m_maxLimit;
+};
+#endif // !__OPERATIONDURATIONHANDLER_H_
