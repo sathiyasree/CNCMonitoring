39cbe80d2091d260ee1e84f5f9bd10b37c27fc30
Sathiyasree <68428401+sathiyasree@users.noreply.github.com>
2020-08-16 21:47:31 +0530

    Create OperationDurationHandler.cpp

---
+++ OperationDurationHandler.cpp
@@ -0,0 +1,24 @@
+#include "OperationDurationHandler.h"
+
+OperationDurationHandler::OperationDurationHandler(unsigned int operationDuration, unsigned int maxLimit)
+{
+	m_operationDuration = operationDuration;
+	m_maxLimit = maxLimit;
+}
+void OperationDurationHandler::updateOperationDuration(unsigned int operationDuration)
+{
+	m_operationDuration = operationDuration;
+	validateCncData();
+}
+void OperationDurationHandler::changeOperationDurationLimit(unsigned int maxLimit)
+{
+	m_maxLimit = maxLimit;
+}
+
+void OperationDurationHandler::validateCncData()
+{
+	if (m_operationDuration > m_maxLimit)
+	{
+		// call Alert function
+	}
+}
