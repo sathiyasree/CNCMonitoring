859b9e776ed2442e7d58c6d5cd21bf2994c6a5c8
Sathiyasree <68428401+sathiyasree@users.noreply.github.com>
2020-08-17 08:18:05 +0530

    Update StartupSelfTestHandler.cpp

--- StartupSelfTestHandler.cpp
+++ StartupSelfTestHandler.cpp
@@ -1,28 +1,29 @@
 #include "StartupSelfTestHandler.hpp"
+#include <map>
+
+std::map<SelfTestStatusCode, std::string> selfTestCodeEnumtoString =
+{
+	{ C_NoData,"No Data available in CNC monitor" },
+	{ C_ControllerBoard_NOK,"Controller board in the machine is not ok" },
+	{ C_ConfigDataCorrupted,"Configuration data in the machine is corrupted"},
+	{ C_AllOk,"All ok, No problem in either CNC machine and in environment" }
+};
+StartupSelfTestHandler::StartupSelfTestHandler(unsigned int selfTestStatuscode)
+{
+	m_selfTestStatuscode =(SelfTestStatusCode) selfTestStatuscode;
+}
+
 void StartupSelfTestHandler::updateStartupSelfTestStatus(unsigned int selfTestStatuscode)
 {
-	m_selfTestStatuscode = selfTestStatuscode;
+	m_selfTestStatuscode = (SelfTestStatusCode)selfTestStatuscode;
 	validateCncData();
 }
 
 void StartupSelfTestHandler::validateCncData()
 {
-	std::string alertText = "";
-	switch (m_selfTestStatuscode)
+	std::string alertText = selfTestCodeEnumtoString[m_selfTestStatuscode];
+	if (m_selfTestStatuscode != C_AllOk)
 	{
-	case C_NoData:
-		alertText = "No Data available in CNC monitor";
-		break;
-	case C_ConfigDataCorrupted:
-		alertText = "Controller board in the machine is not ok";
-		break;
-	case C_ControllerBoard_NOK:
-		alertText = "Configuration data in the machine is corrupted";
-		break;
-	case C_AllOk:
-	default:
-		alertText = "All ok, No problem in either CNC machine and in environment";
-		break;
+		// call alert function
 	}
-	// call alert function
 }
