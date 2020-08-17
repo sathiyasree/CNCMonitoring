be66f65e3e85869b37bc830130ccd2d16b554d34
Sathiyasree <68428401+sathiyasree@users.noreply.github.com>
2020-08-17 12:26:28 +0530

    Update CncMonitor.cpp

--- CncMonitor.cpp
+++ CncMonitor.cpp
@@ -10,10 +10,24 @@
 int main()
 {
     // Below object creation or function calls will be triggerend by timing events 
-    StartupSelfTestHandler StartupSelfTest(C_AllOk);
-    OperatingTempHandler OperatingTemp(23, 35.0f);
-    OperationDurationHandler OperationDuration(30,360);
-    PartDimVariationHandler PartDimVariation (0.0f, 0.05f);
+	// Below object creation or function calls will be triggerend by timing events 
+
+	//Triggered during startup
+	StartupSelfTestHandler StartupSelfTest(C_AllOk);
+
+	// Timer event when 30 minute duration is expired
+	OperatingTempHandler OperatingTemp(23, 35.0f);
+
+	// Timer event when 15 minute duration is expired
+	OperationDurationHandler OperationDuration(30, 360);
+
+	//Assumption: every time a part is produced
+	PartDimVariationHandler PartDimVariation(0.0f, 0.05f);
+
+	PartDimVariation.updatePartDimVariation(0.06f);
+
+
+	return 0;
     
     return 0;
 }
