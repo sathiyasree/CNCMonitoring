d11187d71e382e7ddb3dd9503ec2c25f02f3c2d6
Sathiyasree <68428401+sathiyasree@users.noreply.github.com>
2020-08-17 09:15:52 +0530

    Update OperatingTempHandler.cpp

--- OperatingTempHandler.cpp
+++ OperatingTempHandler.cpp
@@ -10,7 +10,8 @@ void OperatingTempHandler::validateCncData()
 {
 	if (m_currentTemp > m_maxLimit)
 	{
-		//IAlert("Current Operating Temperature: " + std::to_string(m_currentTemp) + " exceeding max limit of " + std::to_string(m_maxLimit));
+		std::string alertText= "Current Operating Temperature : " + std::to_string(m_currentTemp) + " exceeding max limit of " + std::to_string(m_maxLimit);
+		IAlert::getAlertFunction()->alertUser(C_CheckEnvironment, alertText);
 	}
 }
 void OperatingTempHandler::changeOperatingTempLimit(float maxLimit)
