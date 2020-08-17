af99d7f7b74231fceb63b1cbdcb79dcc6bbf7315
Sathiyasree <68428401+sathiyasree@users.noreply.github.com>
2020-08-17 08:21:11 +0530

    Update StartupSelfTestHandler.cpp

--- StartupSelfTestHandler.cpp
+++ StartupSelfTestHandler.cpp
@@ -1,21 +1,21 @@
 #include "StartupSelfTestHandler.hpp"
 #include <map>
 
-std::map<SelfTestStatusCode, std::string> selfTestCodeEnumtoString =
+std::map<SelfTestStatusCodeType, std::string> selfTestCodeEnumtoString =
 {
 	{ C_NoData,"No Data available in CNC monitor" },
 	{ C_ControllerBoard_NOK,"Controller board in the machine is not ok" },
 	{ C_ConfigDataCorrupted,"Configuration data in the machine is corrupted"},
 	{ C_AllOk,"All ok, No problem in either CNC machine and in environment" }
 };
-StartupSelfTestHandler::StartupSelfTestHandler(unsigned int selfTestStatuscode)
+StartupSelfTestHandler::StartupSelfTestHandler(SelfTestStatusCodeType selfTestStatuscode)
 {
-	m_selfTestStatuscode =(SelfTestStatusCode) selfTestStatuscode;
+	m_selfTestStatuscode = selfTestStatuscode;
 }
 
-void StartupSelfTestHandler::updateStartupSelfTestStatus(unsigned int selfTestStatuscode)
+void StartupSelfTestHandler::updateStartupSelfTestStatus(SelfTestStatusCodeType selfTestStatuscode)
 {
-	m_selfTestStatuscode = (SelfTestStatusCode)selfTestStatuscode;
+	m_selfTestStatuscode = selfTestStatuscode;
 	validateCncData();
 }
 
