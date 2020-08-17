19ea6a5c3fd779f6b05f5896a2627444748a7921
Sathiyasree <68428401+sathiyasree@users.noreply.github.com>
2020-08-17 09:19:56 +0530

    Update PartDimVariationHandler.cpp

--- PartDimVariationHandler.cpp
+++ PartDimVariationHandler.cpp
@@ -10,7 +10,8 @@ void PartDimVariationHandler::validateCncData()
 {
 	if (m_currentPartDimVariation > m_maxLimit)
 	{
-		// call Alert function
+		std::string alertText= "Current Dimension Variation : " + std::to_string(m_currentPartDimVariation) + " exceeding max limit of " + std::to_string(m_maxLimit);
+		IAlert::getAlertFunction()->alertUser(C_CheckMachine , alertText);
 	}
 }
 void PartDimVariationHandler::changeOperationTempLimit(float maxLimit)
